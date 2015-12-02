910466e8e2aabdd9471f2903d232f78e08297684
Rostyslav Dzinko <rostislav.dzinko@gmail.com>
2015-12-02 15:49:10 +0200

    Implemented single-select fileopen/filesave/createfolder/selectfolder dialogs for GTK toolkit

--- src/native_dialogs.nim
+++ src/native_dialogs.nim
@@ -4,14 +4,18 @@
 import os
 import strutils
 
-type ErrorUnsupportedPlatform* = ref object of Exception
+type
+  ErrorUnsupportedPlatform* = ref object of Exception
   ## OS is not supported by the library exception
 
-type WindowToolkitKind* {.pure.} = enum  ## Kind of API to call dialogs via
-  Win32 = 0
-  Darwin
-  GTK
-  KDE
+  WindowToolkitKind* {.pure.} = enum  ## Kind of API to call dialogs via
+    Win32 = 0
+    Darwin
+    GTK
+    KDE
+
+  DialogButtonInfo* = tuple[title: string, responseType: int]
+    ## Defines dialog button
 
 # ======= #
 #  LINUX  #
@@ -34,21 +38,62 @@ when defined(linux):
     # Initializing KDE-based environment
     windowToolkitKind = WindowToolkitKind.KDE
 
-  proc callDialogFileOpen*(): seq[string] =
+  let
+    dialogFileOpenDefaultButtons*: seq[DialogButtonInfo] = @[
+      (title: "Cancel", responseType: ResponseType.CANCEL.int),
+      (title: "Open", responseType: ResponseType.ACCEPT.int)
+    ]
+
+    dialogFileSaveDefaultButtons*: seq[DialogButtonInfo] = @[
+      (title: "Cancel", responseType: ResponseType.CANCEL.int),
+      (title: "Save", responseType: ResponseType.ACCEPT.int)
+    ]
+
+    dialogFolderCreateDefaultButtons*: seq[DialogButtonInfo] = @[
+      (title: "Cancel", responseType: ResponseType.CANCEL.int),
+      (title: "Create", responseType: ResponseType.ACCEPT.int)
+    ]
+
+    dialogFolderSelectDefaultButtons*: seq[DialogButtonInfo] = @[
+      (title: "Cancel", responseType: ResponseType.CANCEL.int),
+      (title: "Open", responseType: ResponseType.ACCEPT.int)
+    ]
+
+  proc callDialogFile(action: FileChooserAction, title: string, buttons: seq[DialogButtonInfo] = @[]): string =
+    # Setup dialog
+    var dialog = file_chooser_dialog_new(title.cstring, nil, action, nil)
+    # Setup buttons
+    for button in buttons:
+      discard dialog.add_button(button.title.cstring, button.responseType.cint)
+
+    # Run dialog
+    var res = dialog.run()
+
+    # Analyze call results
+    case ResponseType(res):
+    of ResponseType.ACCEPT, ResponseType.YES, ResponseType.APPLY:
+      let fileChooser = cast[FileChooser](pointer(dialog))
+      return $fileChooser.get_filename()
+    of ResponseType.REJECT, ResponseTYPE.NO , ResponseType.CANCEL, ResponseType.CLOSE:
+      return nil
+    else:
+      return nil
+
+  proc callDialogFileOpen*(title: string, buttons: seq[DialogButtonInfo] = dialogFileOpenDefaultButtons): string =
     ## Calls Linux-based OS open file dialog, and returns selected filename[s]
-    result = @[]
+    result = callDialogFile(FileChooserAction.OPEN, title, buttons)
 
-  proc callDialogFileSave*(): string =
+  proc callDialogFileSave*(title: string): string =
     ## Calls Linux-based OS save file dialog, and returns filename to save to
-    result = nil
+    return callDialogFile(FileChooserAction.SAVE, title, dialogFileSaveDefaultButtons)
 
-  proc callDialogFolderCreate*(): string =
+  proc callDialogFolderCreate*(title: string): string =
     ## Call native Linux-based OS folder creation dialog, and returns folder name
-    result = nil
+    return callDialogFile(FileChooserAction.CREATE_FOLDER, title, dialogFolderCreateDefaultButtons)
 
-  proc callDialogFolderOpen*(): string =
+  proc callDialogFolderSelect*(title: string): string =
     ## Call native Linux-base OS folder opening dialog, and return folder name
-    result = nil
+    return callDialogFile(FileChooserAction.SELECT_FOLDER, title, dialogFolderSelectDefaultButtons)
 
 # ======== #
 # WINDOWS  #
@@ -69,3 +114,7 @@ else:
 
 when isMainModule:
   echo "Your window toolkit is: ", windowToolkitKind
+  echo callDialogFileOpen("Open Simulation Results File")
+  echo callDialogFileSave("Save Simulation Results File")
+  echo callDialogFolderCreate("Create New Folder")
+  echo callDialogFolderSelect("Open Folder")
